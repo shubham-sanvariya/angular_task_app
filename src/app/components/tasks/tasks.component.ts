7ed9783a6b1a16faabc4cfdba0b8bed4d5e97776
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-03-25 21:05:04 +0530

    adding task item component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Task } from '../../Task';
 import { TASKS } from '../../mock-tasks';
 import { NgFor } from '@angular/common';
+import { TaskItemComponent } from '../task-item/task-item.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,TaskItemComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })

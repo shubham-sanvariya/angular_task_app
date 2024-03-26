aee4fbefa1d5862b064f1fac0e24695f9f087582
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-03-26 21:28:44 +0530

    adding font awesome with fa icons

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,13 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../../Task';
 
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgStyle } from '@angular/common';
+
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule,NgStyle],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
 export class TaskItemComponent {
   @Input() task: Task | undefined;
+  faTimes = faTimes;
 }

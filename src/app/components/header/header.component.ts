972a39ce01ef0be4799e26dea6b77d56910a4c91
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-03-31 16:58:36 +0530

    calling ui service onToggle method

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
+import { Subscription } from 'rxjs';
+import { UiService } from '../../services/ui.service';
 
 @Component({
   selector: 'app-header',
@@ -10,8 +12,19 @@ import { ButtonComponent } from '../button/button.component';
 })
 export class HeaderComponent {
   title: string = 'Task App';
+  showAddTask : boolean = false;
+  subscription : Subscription | undefined;
+
+  constructor(private uiService : UiService){
+    this.subscription = this.uiService
+    .onToggle()
+        .subscribe({
+          next : (value) => {this.showAddTask = value},
+          error : (error) => {console.log(error)}
+        })
+  }
 
   toggleAddTask(){
-    console.log('toggle')
+    this.uiService.toggleAddTask();
   }
 }
